9ddb0345a7b692169e7f9823cce2792421b08001
bereeza <timofejberezanskij@gmail.com>
2026-07-03 00:30:27 +0300

    [LEM-11] Updated log logic & Updated handler logic for Native

---
+++ src/main/java/com/lemon/app/LemonApp.java
@@ -0,0 +1,12 @@
+package com.lemon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemonApp.class, args);
+    }
+}
